a3e828ec4f26a5f26fae324c78e7ab72313c7b32
Brandom Vasquez <negagen@aol.com>
2020-06-14 15:17:16 -0500

    Initial Commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using Vidly.Models;
+using Vidly.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private List<Customer> customers = new List<Customer>
+            {
+                new Customer { ID = 1, Name = "John Smith"},
+                new Customer { ID = 2, Name = "Mary Williams"}
+            };
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(customers);
+        }
+
+        [Route("Customers/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customer = customers.Find(x => x.ID == id);
+            if (customer != null) { 
+                return View(customer);
+            }
+            return HttpNotFound();
+        }
+    }
+}
---
+++ Vidly/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Vidly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie{ get; set; }
+        public List<Customer> Customers { get; set; }
+
+    }
+}
